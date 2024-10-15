6bf3d6a07b531693fb904a7a8c60cebf189becb7
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-10-15 07:56:41 +0200

    config: add jest and eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: { ...globals.jest }, 
+    },
+    plugins: {
+      jest: pluginJest,
+    },
+    rules: {
+      ...pluginJest.configs.recommended.rules,
+    },
+  },
   eslintConfigPrettier,
-];
+]
