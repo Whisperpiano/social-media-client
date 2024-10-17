2043cbb426d1d41d21e94b44df2ab9f4eea64dcf
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-10-17 07:20:06 +0200

    feat: add all cypress tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,53 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('visitHome', () => {
+  cy.visit('/')
+  cy.wait(500)
+})
+
+Cypress.Commands.add('showsRegisterForm', () => {
+  cy.get('#registerForm').should('be.visible')
+  cy.wait(500)
+})
+
+Cypress.Commands.add('showsLoginForm', () => {
+  cy.get('#registerForm').find('button[data-auth=login]').click()
+  cy.get('#loginForm').should('be.visible')
+  cy.wait(500)
+})
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.get('#loginForm').find('input[name=email]').type(email)
+  cy.get('#loginForm').find('input[name=password]').type(password)
+  cy.get('#loginForm').find('button[type=submit]').click()
+  cy.wait(1500)
+})
+
+Cypress.Commands.add('loginWithTestUser', () => {
+  cy.fixture('userTest').then((user) => {
+    cy.login(user.email, user.password)
+  })
+  cy.wait(500)
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.get('button[data-auth=logout]').click()
+  cy.wait(500)
+})
+
+Cypress.Commands.add('isLoggedIn', () => {
+  cy.get('button[data-auth=logout]').should('exist')
+  cy.window().then((win) => {
+    expect(win.localStorage.getItem('token')).to.not.be.null
+  })
+  cy.wait(500)
+})
+
+Cypress.Commands.add('isNotLoggedIn', () => {
+  cy.window().then((win) => {
+    expect(win.localStorage.getItem('token')).to.be.null
+  })
+  cy.wait(500)
+})
